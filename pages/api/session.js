132266f7a4e8b6cb9a0a83e6ba7ac3c0649d7677
Joy8824 <joydisplays8@outlook.com>
2025-06-04 14:11:03 -0500

    Update session.js

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const { session } = req.query;
-  if (!session) return res.status(400).json({ error: 'Missing session ID' });
+  const { sessionId } = req.query;
+  if (!sessionId) return res.status(400).json({ error: 'Missing sessionId ID' });
 
   const resp = await fetch(
-    `https://hook.us2.make.com/s0n9gbnzbh2on44e2329v5llt6lqgi3f?sessionId=${session}`
+    `https://hook.us2.make.com/s0n9gbnzbh2on44e2329v5llt6lqgi3f?sessionId=${sessionId}`
     
   );
 
